144e136c20e0fe42e6cf54cb5258560397091cd7
Lutsyuk Victoria <victorialutsyuk.t@gmail.com>
2025-06-15 12:20:32 +0300

    threeSum

---
+++ src/src/main/java/solutions/Solution.java
@@ -0,0 +1,31 @@
+package solutions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        int len = nums.length;
+        List<List<Integer>> result = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                map.putIfAbsent(nums[i], i);
+                map.putIfAbsent(nums[j], j);
+                int rest = - nums[i] - nums[j];
+
+                Integer index = map.get(rest);
+                if (index != null && index != i && index != j) {
+                    List<Integer> solution = new ArrayList<>(List.of(nums[i], nums[j], nums[index]));
+                    Collections.sort(solution);
+                    if (!result.contains(solution)) {
+                        result.add(solution);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
